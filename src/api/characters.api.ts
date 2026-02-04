7b39009bb74706e57aa5bc7e9b5e10379c7190a1
Sanskrati Agrawal <55913586+sanskrati-1234@users.noreply.github.com>
2026-02-04 22:22:35 +0530

    Update characters.api.ts

--- src/api/characters.api.ts
+++ src/api/characters.api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import type { Character } from "../types/character.ts";
 
+const API_BASE =
+  import.meta.env.VITE_API_URL ?? "http://localhost:4000";
+
 export const fetchCharacters = async (): Promise<Character[]> => {
-  const res = await axios.get("http://localhost:4000/characters");
+  const res = await axios.get(`${API_BASE}/characters`);
   return res.data;
 };
